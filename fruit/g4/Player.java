f947dc2067e7f50be7e2f0a2c3e452a3dd47d873
Ryder Moody <rydthemoodster@gmail.com>
2013-10-23 01:00:28 -0400

    Added initial strategy

--- fruit/g4/Player.java
+++ fruit/g4/Player.java
@@ -4,14 +4,41 @@ import java.util.*;
 
 public class Player extends fruit.sim.Player
 {
-    public void init(int nplayers, int[] pref) {
+  private int[] prefs;
+  private int[] platter;
+
+  public void init(int nplayers, int[] pref) {
+    prefs = pref;
+    platter = new int[pref.length];
+  }
+
+  public boolean pass(int[] bowl, int bowlId, int round,
+                      boolean canPick,
+                      boolean mustTake) {
+    int score = score(bowl);
+    int numFruits = 0;
+    for (int i = 0 ; i < bowl.length; i++){
+      numFruits += bowl[i];
+    }
+    int[] uniformBowl = new int[bowl.length];
+
+    for (int i = 0 ; i < bowl.length; i++){
+      uniformBowl[i] = numFruits / bowl.length;
     }
 
-    public boolean pass(int[] bowl, int bowlId, int round,
-                        boolean canPick,
-                        boolean musTake) {
-        return random.nextDouble() > 0.5;
-    }
+    int uniformScore = score(uniformBowl);
+    System.out.println("uniformScore: " + uniformScore);
+    System.out.println("score: " + score);
+    return uniformScore < score;
+  }
 
-    private Random random = new Random();
+  private int score(int[] bowl){
+    int score = 0;
+    for (int i = 0 ; i < bowl.length; i++){
+      score += bowl[i] * prefs[i];
+    }
+    return score;
+  }
+
+  private Random random = new Random();
 }
