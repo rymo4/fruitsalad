a9c3cb59297be8fdf0cb089199bcfbc7cc0e0b3f
Ryder Moody <rydthemoodster@gmail.com>
2013-10-23 23:32:59 -0400

    Added variable cutoff for expectation comparison

--- fruit/g4/Player.java
+++ fruit/g4/Player.java
@@ -6,19 +6,29 @@ public class Player extends fruit.sim.Player
 {
   private float[] prefs;
   private float[] platter;
+  private float numFruits = 0;
+  private float bowlsRemaining;
+  private float totalNumBowls;
 
   public void init(int nplayers, int[] pref) {
     prefs = Vectors.castToFloatArray(pref);
     platter = new float[pref.length];
+    bowlsRemaining = (float)(nplayers - getIndex());
+    totalNumBowls = bowlsRemaining;
+    System.out.println(getIndex());
   }
 
-  public boolean pass(int[] bowl, int bowlId, int round,
-                      boolean canPick,
-                      boolean mustTake) {
+  public boolean pass(int[] bowl, int bowlId, int round, boolean canPick, boolean mustTake) {
+    if (!canPick){
+      return false;
+    }
+
+    System.out.println("Number of bowls that will pass: " + totalNumBowls);
+    System.out.println("Number of bowls remaining: " + bowlsRemaining);
     float[] currentBowl = Vectors.castToFloatArray(bowl);
     float score = score(currentBowl);
 
-    float numFruits = Vectors.sum(currentBowl);
+    numFruits = Vectors.sum(currentBowl);
 
     float[] uniformBowl = new float[currentBowl.length];
     for (int i = 0 ; i < bowl.length; i++){
@@ -28,7 +38,20 @@ public class Player extends fruit.sim.Player
 
     System.out.println("Uniform Score: " + uniformScore);
     System.out.println("Score: " + score);
+    bowlsRemaining--;
     return uniformScore < score;
+  }
+
+  private boolean shouldTakeBasedOnScore(float currentScore, float mle){
+    // based on number of bowls remaining to pass you, decide if you should take
+    if (currentScore < mle) return false;
+
+    float diff = maxScore() - mle;
+    return currentScore > (0.5f * diff * (totalNumBowls / bowlsRemaining)) + mle;
+  }
+
+  private float maxScore(){
+    return numFruits * 12;
   }
 
   private float score(float[] bowl){
